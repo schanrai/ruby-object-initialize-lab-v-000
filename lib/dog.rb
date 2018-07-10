904de9f922538f867f429e04a978a907defb41a4
Sushi Chanrai <sushi@pathworks.co>
2018-07-10 01:00:48 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+  def initialize(dogsname, dogsbreed ="Mutt")
+    @name = dogsname
+    @breed = dogsbreed
+  end
+
+  def name= (dogsname)
+    @name = dogsname
+  end
+
+  def name
+    @name
+  end
+
+  def breed= (dogsbreed)
+    @breed = dogsbreed
+  end
+
+  def breed
+    @breed
+  end
+
+end
